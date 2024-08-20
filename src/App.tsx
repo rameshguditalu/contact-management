2d11afc8ae029732db590cbd7c68451d374e2bde
ramesh.guditalu <ramesh.guditalu@gobrightdrop.com>
2024-08-20 22:31:46 +0530

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { QueryClient, QueryClientProvider } from "react-query";
 import Header from "./components/Header";
